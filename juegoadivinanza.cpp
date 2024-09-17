b892c0be813bf12ce7de9beab7b8f81dd6e90953
jaac-punk <jorge.alcocer@ucb.edu.bo>
2024-09-17 10:15:55 -0400

    2024-09-17-02 segunda version incluye numero_secreto

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+std::cout<<"**************************************"<<endl;
+std::cout<<"*Bienvenido al Juego de la Adivinanza*!"<<endl;
+std::cout<<"**************************************"<<endl;
+
+ int numero_secreto = 42;
+ cout << "el numero secreto es ...."<< numero_secreto << "no lo diga nadie" << endl;
+}
+
+
