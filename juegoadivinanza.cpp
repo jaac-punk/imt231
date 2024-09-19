deff4c872bcf00432749856ab26c2b5b875e7d44
jaac-punk <jorge.alcocer@ucb.edu.bo>
2024-09-19 11:22:10 -0400

    2024-09-19-01 controla siez= >,<

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -8,6 +8,20 @@ std::cout<<"**************************************"<<endl;
 
  int numero_secreto = 42;
  cout << "el numero secreto es ...."<< numero_secreto << "no lo diga nadie" << endl;
+
+ int adivina;
+ cout<< "cual es el numero?";
+ cin >> adivina;
+ cout <<"El valor de su numero es:" << adivina <<endl"
+ if (adivina== numero_secreto){
+     cout << "Felicidades!!! ...... adivina el numero secreto"<< endl;
+      }
+      else if (adivina>numero_secreto){
+          cout << "El numero ingresadoes mayor que el numero secreto" << endl;
+      }
+      else {
+          cout <<"El numero ingresado es menor que el numero secreto" << endl;
+      }
 }
 
 
