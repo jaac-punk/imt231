ab84b5840789e90f0ffd95ed5c018cab0cb628b6
jaac-punk <jorge.alcocer@ucb.edu.bo>
2024-09-24 09:19:00 -0400

    2024-09-24-01 controla ciclo hasta adivinar numero

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 int main(){
-
 std::cout<<"**************************************"<<endl;
 std::cout<<"*Bienvenido al Juego de la Adivinanza*!"<<endl;
 std::cout<<"**************************************"<<endl;
@@ -9,15 +8,20 @@ std::cout<<"**************************************"<<endl;
 const int NUMERO_SECRETO = 42;
 int adivina;
 
- cout<< "cual es el numero?";
- cin >> adivina;
- cout <<"El valor de su numero es:" << adivina <<endl"
+ bool no_acerto = true;
 
- bool acerto = adivina == NUMERO_SECRETO;
- bool mayor = adivina > NUMERO_SECRETO;
+ while (no_acerto){
 
- if(acerto){
-    cout << "Felicidades!!! ...... adivina el numero secreto"<< endl;
+     cout<< "cual es el numero?";
+     cin >> adivina;
+     cout <<"El valor de su numero es:" << adivina << endl;
+
+     bool acerto = adivina == NUMERO_SECRETO;
+     bool mayor = adivina > NUMERO_SECRETO;
+
+     if(acerto){
+          cout << "Felicidades!!! ...... adivina el numero secreto"<< endl;
+          no_acerto = false;
       }
      else if(mayor){
           cout << "El numero ingresado es mayor que el numero secreto" << endl;
@@ -25,6 +29,11 @@ int adivina;
           else{
           cout <<"El numero ingresado es menor que el numero secreto" << endl;
       }
-}
 
+
+ }
+
+cout << "finalizo el juego"<< endl;
+
+}
 
