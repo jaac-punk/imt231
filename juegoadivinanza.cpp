d92fe87c95715efe100b080598d979f44bae7c02
jaac-punk <jorge.alcocer@ucb.edu.bo>
2024-10-01 08:42:16 -0400

    2024-10-01-01 contempla en nivel de dificultad facil, medio, dificil

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,51 +1,75 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-int main(){
-std::cout<<"**************************************"<<endl;
-std::cout<<"*Bienvenido al Juego de la Adivinanza*!"<<endl;
-std::cout<<"**************************************"<<endl;
 
-const int NUMERO_SECRETO = 42;
-int adivina;
+int main() {
+    std::cout << "*****************************************" << endl;
+    std::cout << "*Bienvenido al Juego de la Adivinanza   *!" << endl;
+    std::cout << "*Ingrese un Numero en el Rango : 1 a 100*!" << endl;
+    std::cout << "*****************************************" << endl;
 
- bool no_acerto = true;
- int intentos = 0;
- double puntos = 1000.0; // ...guardar los puntos ganados
+    char dificultad;
+    int numero_tentativas;
 
- while (no_acerto){
-    intentos ++;
-     cout<< "Tentativa" << intentos << endl;
-     cout<< "cual es el numero?" << endl;
-     cin >> adivina;
-     double puntos_perdidos = abs(adivina - NUMERO_SECRETO)/2.0;
-     puntos = puntos - puntos_perdidos;
-     cout << "El valor de su  numero es: "<< adivina << endl;
+    do {
+        cout << endl;
+        cout << "Escoja el nivel de dificultad" << endl;
+        cout << "Facil (f), Medio (m) o Dificil (d)" << endl;
+        cin >> dificultad;
 
-     bool acerto = adivina == NUMERO_SECRETO;
-     bool mayor = adivina > NUMERO_SECRETO;
+        if (dificultad == 'f') {
+            numero_tentativas = 15;
+        } else if (dificultad == 'm') {
+            numero_tentativas = 10;
+        } else if (dificultad == 'd') {
+            numero_tentativas = 5;
+        } else {
+            cout << "Dificultad no válida. Intente nuevamente." << endl;
+        }
+    } while (dificultad != 'f' && dificultad != 'm' && dificultad != 'd');
 
-     if(acerto){
-          cout << "Felicidades!!! ...... adivina el numero secreto"<< endl;
-          no_acerto = false;
-      }
+    srand(time(0));
+    const int NUMERO_SECRETO = 1 + rand() % 100; // genera un número aleatorio entre 1 y 100
 
-     else if(mayor){
-          cout << "El numero ingresado es mayor que el numero secreto" << endl;
+    int adivina;
 
-     }
+    bool no_acerto = true;
+    int intentos = 0;
+    double puntos = 1000.0; // guardar los puntos ganados
 
-    else{
-     cout <<"El numero ingresado es menor que el numero secreto" << endl;
+    while (no_acerto && intentos < numero_tentativas) {
+        intentos++;
+        cout << "Tentativa " << intentos << " de " << numero_tentativas << endl;
+        cout << "¿Cuál es el número?" << endl;
+        cin >> adivina;
 
-      }
+        double puntos_perdidos = abs(adivina - NUMERO_SECRETO) / 2.0;
+        puntos -= puntos_perdidos;
+        cout << "El valor de su número es: " << adivina << endl;
 
+        bool acerto = adivina == NUMERO_SECRETO;
+        bool mayor = adivina > NUMERO_SECRETO;
 
- }
+        if (acerto) {
+            cout << "¡Felicidades! Adivinaste el número secreto." << endl;
+            no_acerto = false;
+        } else if (mayor) {
+            cout << "El número ingresado es mayor que el número secreto." << endl;
+        } else {
+            cout << "El número ingresado es menor que el número secreto." << endl;
+        }
+    }
 
-cout << "finalizo el juego"<< endl;
-cout << "adivina el numero secreto en" << intentos <<"intentos." << endl;
-cout.precision(2);
-cout << fixed;
-cout <<" su puntuacion fue de " << puntos << "puntos" << endl;
+    if (!no_acerto) {
+        cout << "¡Felicidades! Adivinaste el número secreto en " << intentos << " intentos." << endl;
+    } else {
+        cout << "Se agotaron las tentativas. El número secreto era " << NUMERO_SECRETO << "." << endl;
+    }
+
+    cout.precision(2);
+    cout << fixed;
+    cout << "Su puntuación fue de " << puntos << " puntos." << endl;
+
+    return 0;
 }
-
